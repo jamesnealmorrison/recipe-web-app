24ff7d1a529a96828b778ee3b088097aca38eddc
Jim Morrison <jamesnealmorrison@users.noreply.github.com>
2021-02-27 08:46:24 -0600

    Using react-select.

--- src/js/shared/components/Select/Select.style.js
+++ src/js/shared/components/Select/Select.style.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components'
 import ReactSelect from 'react-select'
 
-export const StyledDesktopSelect = styled(ReactSelect)`
+export const StyledSelect = styled(ReactSelect)`
   width: 200px;
   flex-grow: 1;
   font-size: 1rem;
--- src/js/shared/components/Select/index.js
+++ src/js/shared/components/Select/index.js
@@ -1,76 +1,23 @@
 import React from 'react'
 import compose from 'lodash/fp/flowRight'
-import DesktopSelect from './DesktopSelect'
 import PropTypes from 'prop-types'
 import { list } from 'react-immutable-proptypes'
 import allPass from 'lodash/fp/overEvery'
+import Select from 'react-select'
+
+    const options = [
+                   { value: 'chocolate', label: 'Chocolate' },
+                   { value: 'strawberry', label: 'Strawberry' },
+                   { value: 'vanilla', label: 'Vanilla' }
+                 ]
 
 export class SelectBase extends React.PureComponent {
-    static propTypes = {
-        options: PropTypes.oneOfType([list, PropTypes.array]),
-        value: PropTypes.any,
-        valueKey: PropTypes.string,
-        labelKey: PropTypes.string,
-    }
 
-    static defaultProps = {
-        noResultsText: 'No Results Found...',
-        descriptionRenderer: () => null,
-        filter: () => true,
-        options: [],
-        placeholder: '',
+    render() {
+        return (
+            <Select options={ options } />
+        )
     }
-
-    constructor(props) {
-        super(props)
-        this.state = {
-            filter: '',
-            value: this.getValueFromProps(props)
-        }
-    }
-
-    getValueFromProps(props) {
-        if (
-            !props.value ||
-            (props.value[props.valueKey] && props.value[props.labelKey])
-        ) {
-            return props.value
-        }
-    }
-
-    UNSAFE_componentWillReceiveProps(nextProps) {
-        console.log('JIM!!! nextProps = ')
-        console.log(nextProps)
-    }
-
-    filterOptions = (options, filter) => {
-        const filterByText = option => {
-            const trimmedFilter = filter.trim('').toLowerCase()
-            return trimmedFilter
-                ? this.props
-                    .safeFormatMessage(option[this.props.labelKey])
-                    .toLowerCase()
-                    .contains(trimmedFilter)
-                : true
-        }
-        console.log('jim. options = ')
-        console.log(options)
-        console.log('jim. this.props = ')
-        console.log(this.props)
-        return options
-//        return options.filter(allPass([this.props.filter, filterByText]))
-    }
-
-  render() {
-    const SelectComponent = DesktopSelect
-    return (
-        <SelectComponent
-            {...this.props}
-            options={this.filterOptions(this.options, this.state.filter)}
-            filterOptions={options => options}
-        />
-    )
-  }
 }
 
 export default SelectBase
